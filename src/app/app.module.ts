a63cedf1af16cfa46ba181f24ee9ea7f5b5f6d70
manish <manis.kasyap@gmail.com>
2018-02-06 23:01:52 +0400

    adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 import { AppComponent } from "./app.component";
 import { DataService } from "./data.service";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, NgbModule.forRoot()],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
